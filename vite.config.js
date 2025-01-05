accf91ec022d61e5fb03f26506b5450c0f856ce7
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2025-01-05 22:22:10 +0700

    Update .gitignore to exclude vite.config.js; modify index.html for URL handling and local storage updates; enhance vite.config.js for improved API proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
 	server: {
 		proxy: {
 			'/server': {
-				target: 'http://localhost:8000', // Laravel API server
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/server/, ''), // Rewrite /server to /
+				// Proxy requests starting with "/api"
+				target: 'http://127.0.0.1:8000/', // The actual API server
+				changeOrigin: true, // Adjusts the `Origin` header to match the target URL
+				rewrite: (path) => {
+					return path.replace(/^\/server/, '');
+				},
 			},
 		},
 	},
